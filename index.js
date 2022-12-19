21646c9cbff761f6a9df3e2a2de357bd85d97289
Rebecca Wollard <rbwollard@gmail.com>
2022-12-18 17:31:00 -0700

    commit tree 5a2a732a1dbe4bd5f285c45d349da993f0a87c72

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution here
+// // Code your solution here
+
+// // const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(namesArray, string){
+    // loose equality operator?? 
+    let drivermatch = namesArray.filter(namesArray => namesArray.toUpperCase()===string.toUpperCase())
+    return !!drivermatch ? drivermatch : undefined;
+    // if (drivermatch) {return drivermatch
+    // } else {return undefined}
+    }
+
+
+function fuzzyMatch (namesArray, letter) { 
+    let driverToString = namesArray.filter(namesArray => namesArray.substring(0,2)=== letter)
+    return !!driverToString ? driverToString : undefined; 
+}
+    // // if (driverToString[0]=== letter) {
+    //     return true
+
+
+function matchName (obj, name) {
+    const array = obj.entries()
+    const driverMatch = obj.filter(array => obj.forEach() === name)
+    return !!driverMatch ? driverMatch : undefined;
+}
+function matchName (namesArray, name) {
+    const driverMatch = namesArray.filter(driverObj => driverObj.name === name)
+    return !!driverMatch ? driverMatch : undefined;
+}
